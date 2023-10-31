c9cc5191391f2c0d21e4447daf02be86bb30b4ec
Fabischaffts <124001856+Fabischaffts@users.noreply.github.com>
2023-10-31 15:11:26 -0300

    implentApi y buscar Planeta de ella

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/Sw/di/ModuloSwApi.kt
@@ -0,0 +1,11 @@
+package ar.edu.unlam.mobile.scaffold.data.Sw.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+
+@Module
+abstract class ModuloSwApi {
+}
---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/kitty/models/Planeta.kt
@@ -0,0 +1,7 @@
+package ar.edu.unlam.mobile.scaffold.domain.kitty.models
+
+data class Planeta(
+    val name: String,
+    val climate: String,
+    val terrain: String
+)
