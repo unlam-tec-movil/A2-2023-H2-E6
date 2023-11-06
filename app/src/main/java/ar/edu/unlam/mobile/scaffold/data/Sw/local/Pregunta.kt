dbe64e77ee7c469834c7dcf9b904f0fa82846bee
Fabischaffts <124001856+Fabischaffts@users.noreply.github.com>
2023-11-05 23:41:22 -0300

    data class

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/Sw/local/Pregunta.kt
@@ -0,0 +1,8 @@
+package ar.edu.unlam.mobile.scaffold.data.Sw.local
+
+data class Pregunta(
+    val pregunta: String= "0",
+    val respuesta1: String ="1" ,
+    val respuesta2: String ="2",
+    val respuesta3: String ="3"
+)
