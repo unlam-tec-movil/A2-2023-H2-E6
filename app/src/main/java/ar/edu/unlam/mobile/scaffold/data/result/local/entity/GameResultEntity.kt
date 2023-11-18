a889764d739e837e444257b46823b50705a70f68
confaweb <103534724+confaweb@users.noreply.github.com>
2023-11-18 18:56:53 -0300

    optionResult viewModel

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/local/entity/GameResultEntity.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/local/entity/GameResultEntity.kt
@@ -6,14 +6,52 @@ import ar.edu.unlam.mobile.scaffold.data.result.model.GameResult
 import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "gameResult")
-abstract class GameResultEntity(
+data class GameResultEntity(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
+    val gameResult: String, val size: Int,
+)
+data class GameResult(
+    val id: Int,
     val gameResult: String,
-) : List<GameResultEntity>
+    val size: Int
+)
 
 fun GameResultEntity.toDomain() = GameResult(
     id = id,
     gameResult = gameResult,
 )
 
+/*
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+package ar.edu.unlam.mobile.scaffold.data.result.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ar.edu.unlam.mobile.scaffold.data.result.model.GameResult
+
+@Entity(tableName = "gameResult")
+data class GameResultEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    val gameResult: String,
+    val size: Int,
+)
+
+data class GameResult(
+    val id: Int,
+    val gameResult: String,
+    val size: Int
+)
+
+fun GameResultEntity.toDomain() = GameResult(
+    id = id,
+    gameResult = gameResult,
+    size = size
+)
+
+
+ */
