9862b0e7a5fbd65cbb5487688055e211f8d79a98
confaweb <103534724+confaweb@users.noreply.github.com>
2023-11-21 01:59:07 -0300

    trying Room

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/local/ResultDao.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/local/ResultDao.kt
@@ -19,3 +19,30 @@ interface ResultDao {
     fun getAllResults(): Flow<List<GameResultEntity>>
 }
 
+/*
+package ar.edu.unlam.mobile.scaffold.data.result.repository
+
+import androidx.room.Entity
+import ar.edu.unlam.mobile.scaffold.core.database.SwDatabase
+import ar.edu.unlam.mobile.scaffold.data.result.local.ResultDao
+import ar.edu.unlam.mobile.scaffold.data.result.local.entity.GameResultEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@Entity
+class GameResultDefaultRepository @Inject constructor(
+    database: SwDatabase,
+) : GameResultRepository {
+    private val resultDao: ResultDao = database.resultDao()
+    val allEntities: Flow<List<GameResultEntity>> = resultDao.getAllResults()
+
+    suspend fun insert(gameResult: GameResultEntity) {
+        resultDao.insert(gameResult)
+    }
+
+    override fun getAllResults(): Flow<List<GameResultEntity>> {
+        return resultDao.getAllResults()
+    }
+}
+
+ */
--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/repository/GameResultDefaultRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/result/repository/GameResultDefaultRepository.kt
@@ -14,23 +14,55 @@ class GameResultDefaultRepository @Inject constructor(
 ) : GameResultRepository {
     private val resultDao: ResultDao = database.resultDao()
     val allEntity: Flow<List<GameResultEntity>> = resultDao.getAllResults()
+    suspend fun insert(gameResult: GameResultEntity) {
+        resultDao.insert(gameResult)
+    }
     override fun getAllResults(): Flow<List<GameResultEntity>> {
-        return flow{
-            resultDao
-        }
+        return resultDao.getAllResults()
     }
 
-    companion object {
-        fun insertGameResult(result: String) {
+    //companion object {
+     //   fun insertGameResult(result: String) {
 
-        }
-    }
+      //  }
+   // }
 }
 
 /*
-@Database(entities = [GameResultEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun resultDao(): ResultDao
+********************
+* class MyRepository(private val myEntityDao: MyEntityDao) {
+    val allEntities: Flow<List<MyEntity>> = myEntityDao.getAllEntities()
+
+    suspend fun insertEntity(entity: MyEntity) {
+        myEntityDao.insertEntity(entity)
+    }
 }
+
+* ++++++++++++++++++++++++++++
+package ar.edu.unlam.mobile.scaffold.data.result.repository
+
+import androidx.room.Entity
+import ar.edu.unlam.mobile.scaffold.core.database.SwDatabase
+import ar.edu.unlam.mobile.scaffold.data.result.local.ResultDao
+import ar.edu.unlam.mobile.scaffold.data.result.local.entity.GameResultEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@Entity
+class GameResultDefaultRepository @Inject constructor(
+    database: SwDatabase,
+) : GameResultRepository {
+    private val resultDao: ResultDao = database.resultDao()
+    val allEntities: Flow<List<GameResultEntity>> = resultDao.getAllResults()
+
+    suspend fun insert(gameResult: GameResultEntity) {
+        resultDao.insert(gameResult)
+    }
+
+    override fun getAllResults(): Flow<List<GameResultEntity>> {
+        return resultDao.getAllResults()
+    }
+}
+
 
  */
